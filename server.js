7f056e513bff8faaebfbc703613a4f8ee6f440b7
Avinash Shrivastav <avinash.shrivastav@revv.co.in>
2026-06-11 17:53:16 +0530

    code commitedk

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-require('dotenv').config();
+require('dotenv').config({ path: `${__dirname}/.env` });
 
 // Global crash guards — keep server alive on unhandled errors
 process.on('uncaughtException', (err) => {
