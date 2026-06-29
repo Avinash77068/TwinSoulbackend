db37f73ff7342cf67f346f7fdb0cb763586bc713
Avinash Shrivastav <avinash.shrivastav@revv.co.in>
2026-06-29 18:08:46 +0530

    ui done

--- src/routes/chat.routes.js
+++ src/routes/chat.routes.js
@@ -4,17 +4,17 @@ const { upload, handleR2Upload } = require('../middleware/upload');
 const c = require('../controllers/chat.controller');
 
 router.get('/messages', protect, c.getMessages);
-router.post('/messages', protect, upload.single('media'), handleR2Upload, c.sendMessage);
+router.put('/messages/read', protect, c.markRead);
+router.put('/messages/:id/pin', protect, c.pinMessage);
 router.delete('/messages/:id', protect, c.deleteMessage);
+router.get('/messages/search', protect, c.searchMessages);
+router.get('/messages/secret', protect, c.getSecretMessages);
+router.get('/messages/pinned', protect, c.getPinnedMessages);
 router.post('/messages/:id/react', protect, c.reactToMessage);
 router.delete('/messages/:id/react', protect, c.removeReaction);
-router.put('/messages/:id/pin', protect, c.pinMessage);
-router.put('/messages/:id/favorite', protect, c.favoriteMessage);
-router.put('/messages/read', protect, c.markRead);
 router.post('/messages/:id/forward', protect, c.forwardMessage);
-router.get('/messages/search', protect, c.searchMessages);
+router.put('/messages/:id/favorite', protect, c.favoriteMessage);
 router.get('/messages/favorites', protect, c.getFavoriteMessages);
-router.get('/messages/pinned', protect, c.getPinnedMessages);
-router.get('/messages/secret', protect, c.getSecretMessages);
+router.post('/messages', protect, upload.single('media'), handleR2Upload, c.sendMessage);
 
 module.exports = router;
